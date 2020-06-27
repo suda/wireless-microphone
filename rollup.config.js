8e95c441fd717616ec4cc2662b7ca79493bca5b2
Wojtek Siudzinski <admin@suda.pl>
2020-06-27 15:57:53 +0200

    Use terser plugin instead of uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
-import { uglify } from 'rollup-plugin-uglify'
+import {terser} from 'rollup-plugin-terser'
 
 export default {
     input: 'src/script.js',
     output: {
         file: 'dist/bundle.js',
         format: 'iife',
-        name: 'webmic'
+        name: 'webmic',
+        plugins: [terser()]
     },
     plugins: [
         postcss(),
         resolve(),
-        commonjs(),
-        uglify()
+        commonjs()
     ]
 }
